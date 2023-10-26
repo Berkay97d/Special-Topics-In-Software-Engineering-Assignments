4e77c8dd44b695797f010653a25fb4b8376d1a6a
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-10-26 16:10:48 +0300

    Initial commit

---
+++ Assignment1/SpecialTopicsInComputerEngineeringLab1/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SpecialTopicsInComputerEngineeringLab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Server server = new Server();
+            UserSender userSender = new UserSender();
+            
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.Spacebar) // SPACE TUŞU YENİ USER YOLLAR
+                    {
+                        userSender.SendRandomUser(server);
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        break; // ESC PROGRAMI KAPAR
+                    }
+                }
+            }
+
+
+        }
+    }
+    
+    public class Server
+    {
+        private float m_MasterClock;
+        private bool m_IsBusy;
+        private int m_numOfCustomer;
+        private List<User> m_Queue = new List<User>();
+        
+        
+        public void OnUserArrived(User user) //FLOWCHART LOGIC'I ON USER ARRIVED FONKSIYONUNDADIR
+        {
+            m_numOfCustomer++;
+            
+            Console.WriteLine("User " + user.GetId() + " is arrived, current number of user is " + m_numOfCustomer);
+            
+            if (!m_IsBusy)
+            {
+                ServiceToUser(user);
+                return;
+            }
+
+            PutUserToQueue(user);
+        }
+        
+        private User GetNextUserFromQueue() //SERVER BUSY OLMAYI BIRAKINCA SIRADAKİ KİSİYE GECER
+        {
+            var nextUser = m_Queue[0];
+            m_Queue.Remove(nextUser);
+            return nextUser;
+        }
+
+        private void PutUserToQueue(User user) //SERVER DOLU ISE SIRAYA KOYAR
+        {
+            m_Queue.Add(user);
+            
+            Console.WriteLine("User " + user.GetId() + " is putted to queue cause server is bussy");
+        }
+
+        private void DepartureUser(User user) //ISI BITEN USERI YOLLAR 
+        {
+            m_numOfCustomer--;
+            
+            Console.WriteLine("User " + user.GetId() + " is departured, current number of user is " + m_numOfCustomer);
+        }
+
+        private void ServiceToUser(User user) //USER'A USERIN BELİRLEDDİĞİ SERVİS SÜRESİ KADAR HİZMET EDER
+        {   
+            DateTime baslangicZamani = DateTime.Now;
+                
+            Console.WriteLine("Servicing to user " + user.GetId());
+
+            while (true)
+            {
+                
+                m_IsBusy = true;
+
+                DateTime bitisZamani = DateTime.Now;
+                TimeSpan gecenSure = Helpers.CalculateTime(baslangicZamani, bitisZamani);
+
+                if (gecenSure.Seconds >= user.GetServiceTime())
+                {
+                    Console.WriteLine("Service finished to user " + user.GetId());
+                    m_IsBusy = false;
+                
+                    DepartureUser(user);
+
+                    if (m_Queue.Count != 0)
+                    {
+                        ServiceToUser(GetNextUserFromQueue());
+                        
+                        return;
+                    }
+                    
+                    Console.WriteLine("There is no user to service");
+                    return;
+                }
+            }
+        }
+        
+    }
+
+    public class User
+    {
+        private int m_Id;
+        private int m_ServiceTime;
+
+        public User(int id, int serviceTime)
+        {
+            m_Id = id;
+            m_ServiceTime = serviceTime;
+        }
+
+        public int GetServiceTime()
+        {
+            return m_ServiceTime;
+        }
+
+        public int GetId()
+        {
+            return m_Id;
+        }
+    }
+
+    public class UserSender
+    {
+        public void SendRandomUser(Server server)
+        {
+            var randomId = Helpers.GetRandomInteger(1, 5000);
+            var randomServiceTime = Helpers.GetRandomInteger(4, 4);
+
+            User user = new User(randomId, randomServiceTime);
+
+            server.OnUserArrived(user);
+        }
+    }
+    
+    public static class Helpers
+    {
+        public static int GetRandomInteger(int min, int max)
+        {
+            if (min > max)
+            {
+                throw new ArgumentException("Minimum değer maksimum değerden büyük olamaz.");
+            }
+
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+        
+        public static TimeSpan CalculateTime(DateTime baslangicZamani, DateTime bitisZamani)
+        {
+            return bitisZamani - baslangicZamani;
+        }
+    }
+}
---
+++ Assignment2/SpecialTopicsInSoftwareEngineeringLab2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpecialTopicsInSoftwareEngineeringLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            var currentTime = 0;
+            var simulationTime = 50;   // Simülasyon'un kaç saniye süreceği
+            var totalEmptyQueueTime = 0;    // Simulasyon sırasında sıranın boş olduğu süre
+            var totalNonemptyQueueTime = 0;     // Simülasyon sırasında sıranın dolu olduğu süre
+
+            var totalCustomerNumber = random.Next(1, 8);    //Simülasyon sırasonda kaç adet müşteri geleceği
+            int[] arriveTimesForCustomers = new int[8];     // Simülasyon sırasında gelecek müşterilerin gelme zamanlarını tutan array
+            List<Customer> queue = new List<Customer>();    
+            bool isBusy = false;
+            int busyFinishTime = 0;   //Server meşgul olmaya başladğında işini ne zaman bitireceğini tutan değer
+            
+            float P0;
+            float P1;
+
+            
+            for (int i = 0; i < totalCustomerNumber; i++)
+            {
+                arriveTimesForCustomers[i] = random.Next(1, simulationTime);     // Gelecek her customer için server'ı meşgul tutma sürelerini belirliyoruz
+            }
+            
+            
+            while (currentTime < simulationTime)    // Simulasyon döngüsü
+            {
+                Console.WriteLine("-------");   // Her saniyeyi consoleda görmek için
+                
+                if (isBusy)     // Server busy ise ne zaman idle'a dönme durumunu kontol et
+                {
+                    if (currentTime >= busyFinishTime)
+                    {
+                        Console.WriteLine("CUSTOMER GONE " + currentTime);
+                        isBusy = false;
+                    }
+                }
+                else if (queue.Count != 0)    // Server busy değilse ve sırada bekleyen varsa sıradakini al
+                {
+                    isBusy = true;
+                    
+                    int busyTime = random.Next(2, 4);   //Server kaç saniye meşgul kalacak
+                    busyFinishTime = currentTime + busyTime;   // Server işini ne zaman bitirecek
+                    totalNonemptyQueueTime+= busyTime ;  // toplam server meşgul süresine ekle
+                    
+                    queue.Remove(queue[0]);     // Sıradan alına customer'ı sıradan çıkartma
+                }
+
+                if (arriveTimesForCustomers.Any(arriveTime => arriveTime == currentTime ) && currentTime != 0)  // Current time'da arrive olan customer var mı kontrolü
+                {
+                    Console.WriteLine("CUSTOMER ARRİVED " + currentTime);
+                    Customer customer = new Customer();     //Yeni gelen customer
+                    
+                    if (isBusy)     // Yeni customer geldiğinde server meşgul mü kontrolü
+                    {
+                        queue.Add(customer);    // Server busy ise customerı queue'ya koy
+                    }
+                    else
+                    {
+                        isBusy = true;      // Server busy değildi ise artık busy
+
+                        int busyTime = random.Next(2, 4);   //Server kaç saniye meşgul kalacak
+                        busyFinishTime = currentTime + busyTime;   // Server işini ne zaman bitirecek
+                        totalNonemptyQueueTime+= busyTime ;  // toplam server meşgul süresine ekle
+                    }
+                }
+                
+                currentTime++;
+            }
+
+            totalEmptyQueueTime = simulationTime - totalNonemptyQueueTime;  // Kaç saniye meşgul oldu ise onu toplam süreden çıkarıp meşgul olmadığı zamanı bulma
+            
+            Console.WriteLine($"\t -------------SIMULATION FINISHED-----------------");
+            Console.WriteLine($"Simülasyon süresi: {simulationTime} saniye");
+            Console.WriteLine($"{simulationTime} saniyelik simülasyon süresi boyunca toplam {totalCustomerNumber} customer geldi");
+            Console.WriteLine($"Server, toplam {totalEmptyQueueTime} saniye boyunca sırada kimse yoktu.");
+            Console.WriteLine($"Server, toplam {totalNonemptyQueueTime} saniye boyunca sıra doluydu.");
+
+            P0 = (float) totalEmptyQueueTime / simulationTime;
+            P1 = (float) totalNonemptyQueueTime / simulationTime;
+            
+            Console.WriteLine($"PO = {P0} ");
+            Console.WriteLine($"P1 = {P1} ");
+        }
+    }
+
+    public class Customer
+    {
+        
+    }
+}
